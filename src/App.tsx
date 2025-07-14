262d84ddbba715bd90d06748ae990056af56103b
Feekkk <zxduke350@gmail.com>
2025-07-15 01:20:38 +0800

    Restore original portfolio Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./lib/theme-context";
-
-// Simple test component
-const SimpleHome = () => {
-  return (
-    <div style={{padding: '20px', backgroundColor: '#f0f0f0', minHeight: '100vh'}}>
-      <h1 style={{color: 'black', fontSize: '24px'}}>Portfolio Test Page</h1>
-      <p style={{color: 'black'}}>If you can see this, the app is working!</p>
-    </div>
-  );
-};
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="portfolio-ui-theme">
       <Router>
         <Routes>
-          <Route path="/" element={<SimpleHome />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </ThemeProvider>
